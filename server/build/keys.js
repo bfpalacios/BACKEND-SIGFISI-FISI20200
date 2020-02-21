aaa825e1bd23249a6ca8fc753039cb23016dc485
erik antony <erik.antony.mg@gmail.com>
2020-02-21 02:04:29 -0500

    version2

---
+++ server/build/keys.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root', //contraseña de tu usuario root 
+        database: 'sigfisi'
+    }
+};
+//# sourceMappingURL=keys.js.map
